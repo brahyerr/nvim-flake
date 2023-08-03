f337046b6b874e877c6551ba00256034fe899f5c
brahyerr <bp1829847@gmail.com>
2023-08-03 01:22:21 -0400

    moonfly.lua > colorscheme.lua and change theme

---
+++ lua/colorscheme.lua
@@ -0,0 +1,12 @@
+-- vim.g.moonflyTransparent = true
+-- vim.g.moonflyCursorColor = true
+-- vim.g.moonflyNormalFloat = true
+-- vim.g.moonflyTerminalColors = true
+-- vim.g.moonflyTransparent = true
+-- vim.g.moonflyUndercurls = true
+-- vim.g.moonflyUnderlineMatchParen = true
+-- vim.g.moonflyVirtualTextColor = true
+-- vim.cmd([[colorscheme moonfly]])
+
+vim.opt.background = "dark" -- set this to dark or light
+vim.cmd.colorscheme "oxocarbon"
--- lua/moonfly.lua
+++
@@ -1,9 +0,0 @@
-vim.g.moonflyTransparent = true
-vim.g.moonflyCursorColor = true
-vim.g.moonflyNormalFloat = true
-vim.g.moonflyTerminalColors = true
-vim.g.moonflyTransparent = true
-vim.g.moonflyUndercurls = true
-vim.g.moonflyUnderlineMatchParen = true
-vim.g.moonflyVirtualTextColor = true
-vim.cmd([[colorscheme moonfly]])
