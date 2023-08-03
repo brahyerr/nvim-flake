516b34c479cd5b16e7045e8fa4a87fdc030579d6
brahyerr <bp1829847@gmail.com>
2023-08-03 01:36:37 -0400

    edit telescope keybind (conflict with file browser)

--- lua/20-telescope.lua
+++ lua/20-telescope.lua
@@ -20,7 +20,7 @@ telescope.setup({
   },
 })
 
-vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers<CR>")
+-- vim.keymap.set("n", "<leader>fb", "<cmd> Telescope buffers<CR>")
 vim.keymap.set("n", "<leader>ff", "<cmd> Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>fg", "<cmd> Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>fh", "<cmd> Telescope help_tags<CR>")
