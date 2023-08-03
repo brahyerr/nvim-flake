09faea0e4ef447bd4e833f4cf48a3656abe2db7c
brahyerr <bp1829847@gmail.com>
2023-08-03 12:41:19 -0400

    add coq_3p config

--- lua/10-coq_nvim.lua
+++ lua/10-coq_nvim.lua
@@ -3,3 +3,10 @@ g.coq_settings = {
 	auto_start = "shut-up",
 	xdg = true,
 }
+
+require("coq_3p") {
+  -- { src = "nvimlua", short_name = "nLUA" },
+  { src = "vimtex",  short_name = "vTEX" },
+  -- { src = "copilot", short_name = "COP", accept_key = "<c-f>" },
+  -- { src = "codeium", short_name = "COD" },
+}
