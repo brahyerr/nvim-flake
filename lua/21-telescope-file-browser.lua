20ec9f946a2dffac4e6c4d00570a33e47b22b6dd
brahyerr <bp1829847@gmail.com>
2023-08-03 01:36:19 -0400

    add telescope-file-browser and startup lua config

---
+++ lua/21-telescope-file-browser.lua
@@ -0,0 +1,4 @@
+require("telescope").load_extension "file_browser"
+
+vim.api.nvim_set_keymap("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
+-- vim.keymap.set("n", "<leader>fb", "<cmd> Telescope file_browser path=%:p:h select_buffer=true<CR>")
---
+++ lua/startup.lua
@@ -0,0 +1,2 @@
+-- either 'dashbord', 'evil' or 'startify'
+require("startup").setup({theme = "evil"})
